6ca7c453393179fe38603ac7929d1c2fccbbfdd5
nitheesh shetty <nitheesh shetty@LAPTOP-5J3SP7BJ>
2022-07-09 12:26:21 +0530

    Add project files.

---
+++ Vending Machine/Vending Machine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vending_Machine_name
+{
+    public class Vending_Machine
+    {
+        IDictionary<string, double> producttoprice = new Dictionary<string, double>();
+        double currentinsertedvalue;
+        public void insertproduct(string productname, double val)
+        {
+            producttoprice.Add(productname, val);
+        }
+        public double getcurrentvalue()
+        {
+            return currentinsertedvalue;
+        }
+        public void insertcoin(double coinval )
+        {
+            currentinsertedvalue = currentinsertedvalue + coinval;
+}
+        public void reset()
+        {
+            currentinsertedvalue = 0;
+        }
+
+    }
+
+    enum Coins
+    {
+        penny=1,
+        Nickel=2,
+        dimes=3,
+        quarter=4
+    }
+}
+
+
